2609610ef2e68bcaf8846a336fa230df50bddce8
DannyVFilms <dannyvfilms@gmail.com>
2025-10-17 18:51:29 -0500

    Initial Commit

---
+++ PlexChannelsTV/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PlexChannelsTV
+//
+//  Created by Daniel von Seckendorff on 10/17/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PlexChannelsTVTests/PlexChannelsTVTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlexChannelsTVTests.swift
+//  PlexChannelsTVTests
+//
+//  Created by Daniel von Seckendorff on 10/17/25.
+//
+
+import Testing
+@testable import PlexChannelsTV
+
+struct PlexChannelsTVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
